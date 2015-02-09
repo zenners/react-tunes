264213f8a456a888ef7ba4be1e0d8f6de3b883aa
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-02-09 11:08:57 -0700

    Add files

---
+++ app/AddChat.js
@@ -0,0 +1,51 @@
+var React = require('react');
+var $ = require('jquery');
+
+var AddChat = React.createClass({
+  getInitialState: function(){
+    return {
+      chat: ''
+    }
+  },
+  addChat: function(){
+    $.ajax({
+      url: this.props.url,
+      type: 'POST',
+      data: JSON.stringify({text: this.state.chat}),
+      beforeSend: function(request) {
+        request.setRequestHeader("X-Parse-Application-Id", '1tNw34UWSqjkyu4byPGV3q1G6hZcYQmYuvqx0abS');
+        request.setRequestHeader("X-Parse-REST-API-Key", 'ALlZ2WvYnreWNPfHQXoRRiDWt0pXkryYINGAzqnc');
+        request.setRequestHeader("Content-Type", 'application/json');
+      },
+      error: function() {
+        debugger;
+        console.log('error on post');
+      },
+      success: function() {
+        console.log('Succes Post');
+      }.bind(this)
+    })
+  },
+  handleSubmit: function(e){
+    if(e.keyCode === 13){
+      this.addChat(this.state.chat);
+      this.setState({
+        chat: ''
+      })
+    }
+  },
+  handleChange: function(e){
+    this.setState({
+      chat: e.target.value
+    })
+  },
+  render: function(){
+    return (
+      <div className="form-group">
+        <input type="text" placeholder="Compose Message" className="form-control" value={this.state.chat} onChange={this.handleChange} onKeyDown={this.handleSubmit} />
+      </div>
+    )
+  }
+});
+
+module.exports = AddChat;
---
+++ app/ChatList.js
@@ -0,0 +1,49 @@
+var React = require('react');
+var $ = require('jquery');
+
+var ChatList = React.createClass({
+  getInitialState: function(){
+    return {
+      chats: []
+    }
+  },
+  getChats: function(){
+    $.ajax({
+      url: this.props.url,
+      type: 'GET',
+      beforeSend: function(request) {
+        request.setRequestHeader("X-Parse-Application-Id", '1tNw34UWSqjkyu4byPGV3q1G6hZcYQmYuvqx0abS');
+        request.setRequestHeader("X-Parse-REST-API-Key", 'ALlZ2WvYnreWNPfHQXoRRiDWt0pXkryYINGAzqnc');
+        request.setRequestHeader("Content-Type", 'application/json');
+      },
+      error: function(data) {
+        console.log('There was an error in getting the chats');
+      },
+      success: function(data) {
+        if (this.isMounted()) {
+          this.setState({
+            chats: data.results
+          });
+        }
+      }.bind(this)
+    })
+  },
+  componentDidMount: function() {
+    this.getChats();
+    setInterval(function(){
+      this.getChats();
+    }.bind(this), 1000)
+  },
+  render: function(){
+    var list = this.state.chats.map(function(item, index){
+       return <li className="list-group-item" key={item.objectId}> {item.text} </li>
+     });
+    return (
+      <ul className="list-group">
+        {list}
+      </ul>
+    )
+  }
+});
+
+module.exports = ChatList;
