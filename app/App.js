5edba866be810c78fb73fab2429ae03d3400f49c
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-02-10 22:54:37 -0700

    Add Files

--- app/App.js
+++ app/App.js
@@ -2,61 +2,31 @@ var React = require('react');
 var SearchItunes = require('./SearchItunes');
 var Griddle = require('griddle-react');
 
-var ImageComponent = React.createClass({
   render: function(){
-    return <img src={this.props.data} />
-  }
-});
-
-var UrlComponent = React.createClass({
-  render: function(){
-    return <a href={this.props.data}> {this.props.rowData.trackName}</a>
-  }
-});
-
-var App = React.createClass({
-  getInitialState: function(){
-    return {
-      data: ''
-    }
-  },
-  updateState: function(info){
-    this.setState({
-      data: info
-    });
-  },
-  render: function(){
-    var griddleMeta = [{columnName: 'trackName',displayName: 'Name'},{columnName: 'artistName',displayName: 'Artist'},{columnName: 'primaryGenreName',displayName: 'Genre'},{columnName: 'artworkUrl100',displayName: 'Artwork',customComponent: ImageComponent},{columnName: 'trackPrice',displayName: 'Price'},{columnName: 'kind',displayName: 'Type'},{columnName: 'trackViewUrl',displayName: 'Online Link',customComponent: UrlComponent}];
-    var styles = {
-      cushion: {
-        margin: 7
-      },
-      topCushion: {
-        marginTop: 49,
-        borderRadius: 0
-      }
-    };
-    var header = this.state.data === ''? "Make a Search" : "Your Search Results";
+    var griddleMeta = [
+      {columnName: 'trackName',displayName: 'Name'},
+      {columnName: 'artistName',displayName: 'Artist'},
+      {columnName: 'primaryGenreName',displayName: 'Genre'},
+      {columnName: 'artworkUrl100',displayName: 'Artwork',customComponent: ImageComponent},
+      {columnName: 'trackPrice',displayName: 'Price'},{columnName: 'kind',displayName: 'Type'},
+      {columnName: 'trackViewUrl',displayName: 'Online Link',customComponent: UrlComponent}
+    ];
     return (
       <span>
         <div className="navbar navbar-default navbar-fixed-top" role="navigation">
           <div className="container">
             <div className="row">
-              <div className="col-sm-6" style={styles.cushion}>
-                <SearchItunes cb={this.updateState} />
+              <div className="col-sm-6">
+                /* SearchItunes goes here passing it updateState as cb */
               </div>
             </div>
           </div>
         </div>
-        <div className="panel panel-default" style={styles.topCushion} >
+        <div className="panel panel-default" >
           <div className="panel-heading">
-            {header}
+            /* Have this say "Make a Search" when the "data" state is an empty string and have it change to "Your Search Results" when it's not */
           </div>
-          <Griddle
-            results={this.state.data}
-            tableClassName="table"
-            columnMetadata={griddleMeta}
-            columns={["trackName", "artistName", "primaryGenreName", "artworkUrl100", "trackPrice", "kind", "trackViewUrl"]} />
+          /* Griddle Component Goes Here */
         </div>
       </span>
     )
--- app/SearchItunes.js
+++ app/SearchItunes.js
@@ -2,57 +2,18 @@ var React = require('react');
 var $ = require('jquery');
 
 var SearchItunes = React.createClass({
-  getInitialState: function(){
-    return {
-      search: '',
-      entity: 'musicTrack'
-    }
-  },
-  propTypes: {
-    cb: React.PropTypes.func.isRequired
-  },
-  handleChange: function(e){
-    this.setState({
-      search: e.target.value
-    })
-  },
-  formatURL: function(){
-    var url = 'https://itunes.apple.com/search?term=';
-    return url + this.state.search + '&entity=' + this.state.entity;
-  },
-  handleSelectChange: function(e){
-    this.setState({
-      entity: e.target.value
-    });
-  },
-  handleSubmit: function(){
-    var url = this.formatURL();
-    $.ajax({
-      url: url,
-      dataType: 'JSONP',
-      error: function(data) {
-        console.log('Error on getting iTunes Data: ', data);
-      },
-      success: function(data) {
-        this.props.cb(data.results);
-      }.bind(this)
-    })
-  },
   render: function(){
     return (
       <div className="row">
         <div className="col-sm-12">
           <div className="input-group-inline col-sm-4">
-            <input type="text" className="form-control" value={this.state.search} onChange={this.handleChange} />
+            /* 'search' input field goes here */
           </div>
           <div className="input-group-inline col-sm-4">
-            <select className="form-control" value={this.state.entity} onChange={this.handleSelectChange}>
-              <option value="musicTrack">Music</option>
-              <option value="movie">Movie</option>
-            </select>
+            /* 'entity' select tag goes here */
           </div>
           <div className="input-group-inline col-sm-4">
-            <button className="btn btn-primary" onClick={this.handleSubmit}> Get Info </button>
+            /*handleSubmit button goes here*/
           </div>
         </div>
       </div>
