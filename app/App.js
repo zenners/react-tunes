5bc26cd5aea5e8ae3a68eabad86d222d8e432700
Daniel Kesler <danielckesler@gmail.com>
2015-02-18 12:04:16 -0700

    Update App.js

--- app/App.js
+++ app/App.js
@@ -2,7 +2,7 @@ var React = require('react');
 var SearchItunes = require('./SearchItunes');
 var Griddle = require('griddle-react');
 
-React.createClass({
+var App = React.createClass({
   render: function(){
     var griddleMeta = [
       {columnName: 'trackName',displayName: 'Name'},
