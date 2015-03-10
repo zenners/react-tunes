126312b353f418fd3ef9c92e5236d2dd18f3c818
zenners <cu.michaels@gmail.com>
2015-03-10 14:53:28 -0700

    finished specs

--- app/App.js
+++ app/App.js
@@ -2,8 +2,49 @@ var React = require('react');
 var SearchItunes = require('./SearchItunes');
 var Griddle = require('griddle-react');
 
+
+var ImageComponent = React.createClass({
+  render: function() {
+    return (
+      <img src={this.props.data} />
+    );
+  }
+});
+
+var Loading = React.createClass({
+  render: function (){
+    return(
+      <span> Loading... </span>
+    )
+  }
+});
+
+var UrlComponent = React.createClass({
+  render: function() {
+    return (
+      <a href={this.props.data}> {this.props.rowData.trackName} </a>
+    );
+  }
+});
+
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      data:'' 
+    };
+  },
+
+  updateState: function(info){
+    this.setState({
+      data: info
+    })
+  },
+
   render: function(){
+    var panelStyle={
+      marginTop: 50,
+      border: 0
+    };
     var griddleMeta = [
       {columnName: 'trackName',displayName: 'Name'},
       {columnName: 'artistName',displayName: 'Artist'},
@@ -18,17 +59,26 @@ var App = React.createClass({
           <div className="container">
             <div className="row">
               <div className="col-sm-6">
-                /* SearchItunes goes here passing it updateState as cb */
+                <SearchItunes cb={this.updateState} />
               </div>
             </div>
           </div>
         </div>
-        <div className="panel panel-default" >
+        <div className="panel panel-default" style={panelStyle}>
           <div className="panel-heading">
-            /* Have this say "Make a Search" when the "data" state is an empty string and have it change to "Your Search Results" when it's not */
+            {!this.state.data ? <span>Make a Search</span> : <span>Your Search results</span>}
           </div>
-          /* Griddle Component Goes Here */
+          <Griddle results={this.state.data} 
+                     tableClassName="table"
+                     columns={["trackName", "artistName", "primaryGenreName", "artworkUrl100", "trackPrice", "trackViewUrl" ]} 
+                     columnMetadata={griddleMeta}
+                     enableInfiniteScroll={true}
+                     resultsPerPage={5}
+                     loadingComponent={Loading}
+                     useFixedHeader={true}
+                     bodyHeight={400} />
         </div>
+          
       </span>
     )
   }
--- app/SearchItunes.js
+++ app/SearchItunes.js
@@ -2,19 +2,50 @@ var React = require('react');
 var $ = require('jquery');
 
 var SearchItunes = React.createClass({
+  propTypes: {
+    cb:React.PropTypes.func
+  },
+
+  formatUrl: function(){
+    var search = this.refs.searchInput.getDOMNode().value;
+    var select = this.refs.selectInput.getDOMNode().value;
+    return 'https://itunes.apple.com/search?term='+search+'&entity='+select;
+  },
+
+  handleSubmit: function(e){
+      e.preventDefault();
+      $.ajax({
+        url: this.formatUrl(),
+        type: 'POST',
+        dataType: 'jsonp',
+        success: function(data){
+          console.log(data);
+          this.props.cb(data.results);
+          this.refs.searchInput.getDOMNode().value = '';
+        }.bind(this)
+      });
+    
+    
+  },
+
   render: function(){
     return (
       <div className="row">
         <div className="col-sm-12">
+          <form onSubmit={this.handleSubmit}>
           <div className="input-group-inline col-sm-4">
-            /* 'search' input field goes here */
+            <input type="text" className="form-control" ref="searchInput" />
           </div>
           <div className="input-group-inline col-sm-4">
-            /* 'entity' select tag goes here */
+            <select ref="selectInput" className="form-control">
+              <option value="musicTrack"> Music </option>
+              <option value="movie"> Movie </option>
+            </select>
           </div>
           <div className="input-group-inline col-sm-4">
-            /*handleSubmit button goes here*/
+            <button  className="btn btn-primary"> Go Find It! </button>
           </div>
+          </form>
         </div>
       </div>
     );
